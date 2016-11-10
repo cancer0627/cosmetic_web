4bb0aafe8c882406f4d993c0b13e0f7229146d97
cui chen <cancer_1942913930@163.com>
2016-11-10 15:57:17 +0800

    login页面重构，静态连接服务器成功

---
+++ cc/public/js/ser_login.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Administrator on 2016/11/10 0010.
+ */
+var login_btn = document.getElementById('login_btn');
+var userName = document.getElementById('login_input').childNodes[1].childNodes[3];
+var userPwd = document.getElementById('login_input').childNodes[3].childNodes[3];
+
+var url = "http://127.0.0.1:3000/";
+
+function sendCmd(type) {
+    var u = url + type;
+    $.post(u, {
+        username: userName.value,
+        userpwd: userPwd.value
+    }, function (data, status) {
+        if (data.result) {
+            alert('login success!!!');
+        }
+        else {
+            alert('login fail!!!');
+        }
+        console.log(data);
+    });
+}
+
+login_btn.onclick = function () {
+    sendCmd('login');
+};
--- cc/routes/index.js
+++ cc/routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
+router.post('/login', function (req, res) {
+    res.contentType('json');
+    console.log(req.body);
+    var params = {
+        username: req.body.username,
+        userpwd: req.body.userpwd
+    };
+    if (params.username == 'admin' && params.userpwd == 'admin') {
+        params.result = true;
+        res.send(JSON.stringify(params));
+        res.end();
+    }
 
+});
 module.exports = router;
